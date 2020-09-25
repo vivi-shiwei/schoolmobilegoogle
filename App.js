9c6abb1b42d0d938e9d9c99541ba3572973a4285
vivi-shiwei <2546710985@qq.com>
2020-09-25 21:59:20 +0800

    第一次

--- App.js
+++ App.js
@@ -1,11 +1,52 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from 'expo-status-bar'
+import React, { useEffect, useState } from 'react'
+import { Button, StyleSheet, Text, View } from 'react-native'
+import * as GoogleSignIn from 'expo-google-sign-in'
 
 export default function App() {
+  const [user, setUser] = useState(null)
+
+  const signOutAsync = async () => {
+    await GoogleSignIn.signOutAsync()
+    setUser(null)
+  }
+
+  const syncUserWithStateAsync = async () => {
+    await GoogleSignIn.initAsync({
+      clientId: '404097902962-quufj5t480gmgr9e9r0brvovankauun0.apps.googleusercontent.com',
+    })
+    const user = await GoogleSignIn.signInSilentlyAsync()
+    setUser({ user })
+  }
+
+  const signInAsync = async () => {
+    try {
+      await GoogleSignIn.askForPlayServicesAsync()
+      const { type, user } = await GoogleSignIn.signInAsync()
+      alert('user' + user.accessToken)
+      alert('333')
+      if (type === 'success') {
+        syncUserWithStateAsync()
+        alert(user)
+      }
+    } catch ({ message }) {
+      alert('login: Error:' + message)
+    }
+  }
+
+  const onPress = () => {
+    if (!!user) {
+      signOutAsync()
+    } else {
+      signInAsync()
+    }
+  }
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text>{!!user ? "已登錄" : "已登出"}</Text>
+      <Text>{JSON.stringify(user)}</Text>
+      <Button title={!!user ? "登出" : "登錄"} onPress={onPress} />
       <StatusBar style="auto" />
     </View>
   );
@@ -16,6 +57,6 @@ const styles = StyleSheet.create({
     flex: 1,
     backgroundColor: '#fff',
     alignItems: 'center',
-    justifyContent: 'center',
+    justifyContent: 'center'
   },
 });
